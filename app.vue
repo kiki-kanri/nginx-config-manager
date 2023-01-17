5c248027a55a67cb7f095f21bb54ef7a61de1e5f
kiki-kanri <a470666@gmail.com>
2023-01-17 23:25:34 +0800

    Add element-plus plugin file and edit app file.

--- app.vue
+++ app.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<NuxtLayout>
+		<Head>
+            <Title>Nginx設定檔管理系統</Title>
+        </Head>
+		<NuxtPage></NuxtPage>
+	</NuxtLayout>
 </template>
+
+<script lang="ts" setup>
+
+	if (!isServer) {
+		// Disable ctrl+s and ctrl+u
+		window.addEventListener('keydown', (event) => {
+			if (checkEventKey(event, 's', { ctrl: true }, true)) event.preventDefault();
+			if (checkEventKey(event, 'u', { ctrl: true }, true)) event.preventDefault();
+		}, false);
+
+		// Disable drop file open newtab
+		document.addEventListener('dragover', (event) => {
+			event.preventDefault();
+		});
+
+		window.addEventListener('drop', (event) => {
+			event.preventDefault();
+		}, false);
+	}
+
+</script>
---
+++ plugins/element-plus.ts
@@ -0,0 +1,8 @@
+import ElementPlus from 'element-plus';
+import zhTW from 'element-plus/dist/locale/zh-tw';
+
+export default defineNuxtPlugin((nuxtApp) => {
+	nuxtApp.vueApp.use(ElementPlus, {
+		locale: zhTW
+	});
+});
