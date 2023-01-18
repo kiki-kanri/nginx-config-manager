d53ade6e3269455e7dc88fa22c0494a92c453917
kiki-kanri <a470666@gmail.com>
2023-01-18 15:30:56 +0800

    Edit axios file.

--- composables/axios.ts
+++ composables/axios.ts
@@ -25,10 +25,10 @@ async function request(
 	method: string,
 	params: IAxiosData = {},
 	data: IAxiosData = {},
-	config: AxiosRequestConfig = {}
+	config?: AxiosRequestConfig
 ): Promise<IAxiosResponse> {
 	return await axiosApi.request({
-		...config,
+		...(config || {}),
 		data,
 		method,
 		params,
@@ -39,54 +39,54 @@ async function request(
 /**
  * Use delete method request.
  */
-export const useDelete = async (url: string, params: IAxiosData = {}, config: AxiosRequestConfig = {}) => {
+export const useDelete = async (url: string, params: IAxiosData = {}, config?: AxiosRequestConfig) => {
 	return await request(url, 'delete', params, {}, config);
 }
 
-export const $useDelete = async (url: string, params: IAxiosData = {}, config: AxiosRequestConfig = {}) => {
+export const $useDelete = async (url: string, params: IAxiosData = {}, config?: AxiosRequestConfig) => {
 	return (await useDelete(url, params, config)).data;
 }
 
 /**
  * Use get method request.
  */
-export const useGet = async (url: string, params: IAxiosData = {}, config: AxiosRequestConfig = {}) => {
+export const useGet = async (url: string, params: IAxiosData = {}, config?: AxiosRequestConfig) => {
 	return await request(url, 'get', params, {}, config);
 }
 
-export const $useGet = async (url: string, params: IAxiosData = {}, config: AxiosRequestConfig = {}) => {
+export const $useGet = async (url: string, params: IAxiosData = {}, config?: AxiosRequestConfig) => {
 	return (await useGet(url, params, config)).data;
 }
 
 /**
  * Use patch method request.
  */
-export const usePatch = async (url: string, data: formType = {}, config: AxiosRequestConfig = {}) => {
+export const usePatch = async (url: string, data: formType = {}, config?: AxiosRequestConfig) => {
 	return await request(url, 'patch', {}, data, config);
 }
 
-export const $usePatch = async (url: string, data: formType = {}, config: AxiosRequestConfig = {}) => {
+export const $usePatch = async (url: string, data: formType = {}, config?: AxiosRequestConfig) => {
 	return (await usePatch(url, data, config)).data;
 }
 
 /**
  * Use post method request.
  */
-export const usePost = async (url: string, data: formType = {}, config: AxiosRequestConfig = {}) => {
+export const usePost = async (url: string, data: formType = {}, config?: AxiosRequestConfig) => {
 	return await request(url, 'post', {}, data, config);
 }
 
-export const $usePost = async (url: string, data: formType = {}, config: AxiosRequestConfig = {}) => {
+export const $usePost = async (url: string, data: formType = {}, config?: AxiosRequestConfig) => {
 	return (await usePost(url, data, config)).data;
 }
 
 /**
  * Use put method request.
  */
-export const usePut = async (url: string, data: formType = {}, config: AxiosRequestConfig = {}) => {
+export const usePut = async (url: string, data: formType = {}, config?: AxiosRequestConfig) => {
 	return await request(url, 'put', {}, data, config);
 }
 
-export const $usePut = async (url: string, data: formType = {}, config: AxiosRequestConfig = {}) => {
+export const $usePut = async (url: string, data: formType = {}, config?: AxiosRequestConfig) => {
 	return (await usePut(url, data, config)).data;
 }
