ab7996ed66348fbd46a864f52ca929a71e880ba2
kiki-kanri <a470666@gmail.com>
2023-03-07 16:44:15 +0800

    Edit main app file.

--- app.vue
+++ app.vue
@@ -6,22 +6,3 @@
 		<NuxtPage></NuxtPage>
 	</NuxtLayout>
 </template>
-
-<script lang="ts" setup>
-
-	// Disable ctrl+s and ctrl+u
-	window.addEventListener('keydown', (event) => {
-		if (checkEventKey(event, 's', { ctrl: true }, true)) event.preventDefault();
-		if (checkEventKey(event, 'u', { ctrl: true }, true)) event.preventDefault();
-	}, false);
-
-	// Disable drop file open newtab
-	document.addEventListener('dragover', (event) => {
-		event.preventDefault();
-	});
-
-	window.addEventListener('drop', (event) => {
-		event.preventDefault();
-	}, false);
-
-</script>
